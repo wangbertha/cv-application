6d53018f0d34c1fa873c9b1ef2f727c8e5f316a8
wangbertha <wangbertha@gmail.com>
2024-08-20 11:05:28 -0500

    Indent levels of object properly

--- src/assets/entryStructure.jsx
+++ src/assets/entryStructure.jsx
@@ -2,84 +2,84 @@ import  { v4 as uuidv4 } from 'uuid';
 
 let generalInfoEntryStructure = {
     meta: {
-    id: uuidv4(),
+        id: uuidv4(),
     },
     firstName: {
-    label: 'First Name',
-    type: 'text',
-    value: ''
+        label: 'First Name',
+        type: 'text',
+        value: ''
     },
     lastName: {
-    label: 'Last Name',
-    type: 'text',
-    value: ''
+        label: 'Last Name',
+        type: 'text',
+        value: ''
     },
     email: {
-    label: 'Email',
-    type: 'email',
-    value: ''
+        label: 'Email',
+        type: 'email',
+        value: ''
     },
     phoneNumber: {
-    label: 'Phone Number',
-    type: 'tel',
-    value: ''
+        label: 'Phone Number',
+        type: 'tel',
+        value: ''
     }
 }
 
 let educationEntryStructure = {
     meta: {
-    id: uuidv4(),
+        id: uuidv4(),
     },
     schoolName: {
-    label: 'School',
-    type: 'text',
-    value: ''
+        label: 'School',
+        type: 'text',
+        value: ''
     },
     degree: {
-    label: 'Degree',
-    type: 'text',
-    value: ''
+        label: 'Degree',
+        type: 'text',
+        value: ''
     },
     major: {
-    label: 'Major',
-    type: 'text',
-    value: ''
+        label: 'Major',
+        type: 'text',
+        value: ''
     },
     graduationYear: {
-    label: 'Graduation Year',
-    type: 'number',
-    value: ''
+        label: 'Graduation Year',
+        type: 'number',
+        value: ''
     }
 }
 
 let workExperienceEntryStructure = {
     meta: {
-    id: uuidv4(),
+        id: uuidv4(),
     },
     companyName: {
-    label: 'Company Name',
-    type: 'text',
-    value: ''
+        label: 'Company Name',
+        type: 'text',
+        value: ''
     },
     positionTitle: {
-    label: 'Position Title',
-    type: 'text',
-    value: ''
+        label: 'Position Title',
+        type: 'text',
+        value: ''
     },
     responsibilities: {
-    label: 'Responsibilities',
-    type: 'text',
-    value: ''
+        label: 'Responsibilities',
+        type: 'text',
+        value: ''
     },
     dateFrom: {
-    label: 'Date From',
-    type: 'date',
-    value: ''
+        label: 'Date From',
+        type: 'date',
+        value: ''
     },
     dateTo: {
-    label: 'Date To',
-    type: 'date',
-    value: ''
+        label: 'Date To',
+        type: 'date',
+        value: ''
     }
 }
 
